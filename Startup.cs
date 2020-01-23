fe62de7d67eb02fc08e07dd1bf5b0d8da282ae89
Orxan <orxan@skybox.studio>
2020-01-23 15:32:34 +0400

    тест

--- Startup.cs
+++ Startup.cs
@@ -14,6 +14,7 @@ namespace pipelines_dotnet_core
 {
     public class Startup
     {
+        //xaos
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
